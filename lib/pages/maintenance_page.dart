0f859d5505ecb9fa0066f8c917d9d80794334601
gabwrel <johngabriel.declaro@wvsu.edu.ph>
2023-05-30 19:43:56 +0800

    navbar pages

---
+++ lib/pages/maintenance_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MaintenancePage extends StatefulWidget {
+  const MaintenancePage({super.key});
+
+  @override
+  State<MaintenancePage> createState() => _MaintenancePageState();
+}
+
+class _MaintenancePageState extends State<MaintenancePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/testnow_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestNowPage extends StatefulWidget {
+  const TestNowPage({super.key});
+
+  @override
+  State<TestNowPage> createState() => _TestNowPageState();
+}
+
+class _TestNowPageState extends State<TestNowPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
