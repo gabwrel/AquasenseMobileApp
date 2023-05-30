988a4a08afc5d3aba2382ab69a93c7be04b6b5de
gabwrel <johngabriel.declaro@wvsu.edu.ph>
2023-05-30 16:43:59 +0800

    added boxitem

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -19,7 +19,7 @@ class _DashboardPageState extends State<DashboardPage> {
   void initState() {
     super.initState();
     initializeFirebase();
-    databaseRef = FirebaseDatabase.instance.reference();
+    databaseRef = FirebaseDatabase.instance.ref();
 
     databaseRef.child('pH').onValue.listen((event) {
       if (event.snapshot.value != null) {
@@ -69,19 +69,255 @@ class _DashboardPageState extends State<DashboardPage> {
   }
 
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard Page'),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+        flexibleSpace: Align(
+          alignment: Alignment.center,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 40),
+            child: Image.asset(
+              'assets/images/logo2.png',
+              height: 80,
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () {
+              // TODO: Implement refresh functionality
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: [
-            Text('pH: $pH'),
-            Text('Water Level: $waterLevel'),
-            Text('Water Temperature: $waterTemp'),
-            Text('Water Turbidity: $waterTurbidity'),
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Aquasense',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                // TODO: Implement Home navigation
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('User'),
+              onTap: () {
+                // TODO: Implement User navigation
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('About Page'),
+              onTap: () {
+                // TODO: Implement About Page navigation
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              onTap: () {
+                // TODO: Implement Logout functionality
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'DASHBOARD',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Water Parameters',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: BoxItem(
+                    icon: Icons.opacity,
+                    title: 'pH Level',
+                    value: '$pH',
+                  ),
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: BoxItem(
+                    icon: Icons.opacity,
+                    title: 'Water Level',
+                    value: '$waterLevel',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: BoxItem(
+                    icon: Icons.thermostat_outlined,
+                    title: 'Temperature',
+                    value: '$waterTemp', // Placeholder value, replace with actual data
+                  ),
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: BoxItem(
+                    icon: Icons.waves_outlined,
+                    title: 'Water Turbidity',
+                    value: '$waterTurbidity', // Placeholder value, replace with actual data
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Environment Controls',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: BoxItem(
+                    icon: Icons.lightbulb,
+                    title: 'Lighting',
+                    value: '--', // Placeholder value, replace with actual data
+                  ),
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: BoxItem(
+                    icon: Icons.restaurant,
+                    title: 'Feeding',
+                    value: '--', // Placeholder value, replace with actual data
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Filtration System',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: BoxItem(
+                    icon: Icons.whatshot,
+                    title: 'Heater',
+                    value: '--', // Placeholder value, replace with actual data
+                  ),
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: BoxItem(
+                    icon: Icons.lightbulb,
+                    title: 'UV Lamp',
+                    value: '--', // Placeholder value, replace with actual data
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BoxItem extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String value;
+
+  const BoxItem({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(icon, size: 24),
+            const SizedBox(width: 8),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  Text(
+                    value,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
