f33bfa495b18f0f6d758d8ed7af63450de77ae8b
gabwrel <johngabriel.declaro@wvsu.edu.ph>
2024-01-07 23:44:07 +0800

    Updated google sheets api

--- lib/pages/previous_readings.dart
+++ lib/pages/previous_readings.dart
@@ -2,10 +2,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
-// import 'package:fl_chart/fl_chart.dart';
 
 class PreviousReadings extends StatefulWidget {
-  const PreviousReadings({super.key});
+  const PreviousReadings({Key? key}) : super(key: key);
 
   @override
   State<PreviousReadings> createState() => _PreviousReadingsState();
@@ -21,90 +20,114 @@ class _PreviousReadingsState extends State<PreviousReadings> {
   }
 
   Future<void> fetchData() async {
-    final response = await http.get(
-      Uri.parse(
-        'https://script.google.com/macros/s/AKfycbwiyxXB3ny8t67gYn57XMadsmql5IYdD_Q5Jvm3TYDnY246U9P6XBbM_zI2dQRGHX2xnQ/exec',
-      ),
-    );
+    try {
+      final response = await http.get(
+        Uri.parse(
+          'https://script.google.com/macros/s/AKfycbwJuWDd9UV-53FuV5kUJYcQodL7zF5PFBcA7qwVJl-L2pHlV6t8h1-B73Et5q79fkbhlg/exec',
+        ),
+      );
 
-    if (response.statusCode == 200) {
-      List<dynamic> data = json.decode(response.body);
-      setState(() {
-        readings = List<Map<String, dynamic>>.from(data);
-      });
-    } else {
-      throw Exception('Failed to load data');
+      if (response.statusCode == 200) {
+        List<dynamic> data = json.decode(response.body);
+        setState(() {
+          readings = List<Map<String, dynamic>>.from(data);
+        });
+      } else {
+        showErrorSnackbar('Failed to load data');
+      }
+    } catch (error) {
+      showErrorSnackbar('An error occurred: $error');
     }
   }
 
-  String formatDateTime(String dateTimeString) {
-    // Parse the string to a DateTime object
-    DateTime dateTime = DateTime.parse(dateTimeString);
-
-    // Format the DateTime as per your requirements
-    String formattedDateTime =
-        DateFormat('yyyy-MM-dd HH:mm:ss').format(dateTime);
-
-    return formattedDateTime;
+  void showErrorSnackbar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: Duration(seconds: 3),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 80,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.blue),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/dashboard');
-          },
-        ),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
+      appBar: buildAppBar(),
+      body: buildBody(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      toolbarHeight: 80,
+      backgroundColor: Colors.white,
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back, color: Colors.blue),
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, '/dashboard');
+        },
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Expanded(child: Container()),
+          Image.asset(
+            'assets/images/logo2.png',
+            height: 60,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildBody() {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
           children: [
-            Expanded(child: Container()),
-            Image.asset(
-              'assets/images/logo2.png',
-              height: 60,
+            Image.asset('assets/images/PreviousReadings.png'),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: buildDataTable(),
             ),
           ],
         ),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset('assets/images/PreviousReadings.png'),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: DataTable(
-                    columns: const [
-                      DataColumn(label: Text('Date and Time')),
-                      DataColumn(label: Text('pH Level')),
-                      DataColumn(label: Text('Temperature')),
-                      DataColumn(label: Text('Water Turbidity')),
-                    ],
-                    rows: readings.map((reading) {
-                      return DataRow(
-                        cells: [
-                          DataCell(
-                              Text(formatDateTime(reading['DateAndTime']))),
-                          DataCell(Text(reading['pHLevel'].toString())),
-                          DataCell(Text(reading['temperature'].toString())),
-                          DataCell(Text(reading['waterTurbidity'].toString())),
-                        ],
-                      );
-                    }).toList(),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
+  }
+
+  DataTable buildDataTable() {
+    return DataTable(
+      columns: const [
+        DataColumn(label: Text('Date')),
+        DataColumn(label: Text('Time')),
+        DataColumn(label: Text('ph Level')),
+        DataColumn(label: Text('Turbidity (NTU)')),
+        DataColumn(label: Text('Temperature (C)')),
+      ],
+      rows: readings
+          .map(
+            (reading) => DataRow(
+              cells: [
+                DataCell(Text(formatDate(reading['Date']))),
+                DataCell(Text(formatTime(reading['Time']))),
+                DataCell(Text(reading['ph Level'].toString())),
+                DataCell(Text(reading['Turbidity (NTU)'].toString())),
+                DataCell(Text(reading['Temperature (C)'].toString())),
+              ],
+            ),
+          )
+          .toList(),
+    );
+  }
+
+  String formatDate(String dateString) {
+    DateTime date = DateTime.parse(dateString);
+    return DateFormat('yyyy-MM-dd').format(date);
+  }
+
+  String formatTime(String timeString) {
+    DateTime time = DateTime.parse(timeString);
+    return DateFormat('HH:mm:ss').format(time);
   }
 }
